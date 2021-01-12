2d6989385cec9f6566b1631744534f531ce66f49
Ellyn <ellyn.bouscasse@gmail.com>
2021-01-12 11:08:02 +0100

    Update basics.rb
    Lecture day01 ruby - batch 549

--- basics.rb
+++ basics.rb
@@ -1,59 +1,66 @@
-puts "hello"
 
 # **************************************************
 
 # INTERPOLATION CONCEPT:
-# injecting ruby code inside of a string thanks to the syntax #{}
-# only works with double quotes
-puts "calculation: #{1+1}"
-puts 'calculation: #{1+1}'
+# injecting ruby code inside of a string thanks to the syntax "something #{ruby_code}"
+# only works with double quotes, does not work with single quotes
+
+age = 20
+
+puts "your age this year is #{age}"
+puts "your age next next year will be #{age + 1}"
+
 
 # **************************************************
 
 # CONCATENATION CONCEPT:
 # putting/adding two strings together
 # works with single quotes and double quotes
-puts 'hello' + ' ' + 'guys' + ' ' + 'good morning'
+
+greeting = 'Good '  + 'morning' + ' ' + '!'
+puts greeting
+
+
 
 # **************************************************
 # VARIABLES
 
-age = 26
-# I *ASSIGN* the integer 26 to the variable called age.
+age = 36
 
-age = 27
-# puts age
+# I assign the integer 20 to the variable called age.
+# read it from right to left => always.
+name = "Fabrizio"
+
+puts "#{name} is #{age} today"
+# I assign the string fabrizio to the variable name.
+
+# I *ASSIGN* an object to the variable called something.
+
+
+# WHY ? use variable to re-assign them and re-use them over your program
+age = age + 1
+# age += 1
+# age -= 1
+puts "Next year #{name} will be #{age}"
 
 # *************************************************
-# INCREMENTATION: incrementing the value of a variable by one and re-assigning it
+# INCREMENTATION: incrementing the value of a variable by one and re-assigning it (reassignement process but for an integer or a float)
 # REASSIGNMENT: reassigning the value of a variable
-age = age + 1
-# i add one to the variable age (which is at the moment 27) and re-assign the variable age to the sum of 27+1. Now age is 28.
 
-# puts age
-
-
-string_bonjour = "bonjour"
-puts string_bonjour
-
-string_bonjour = string_bonjour + ' ' + 'guys'
-# I *RE-ASSIGN* the variable called string_bonjour by adding the string 'guys'
-puts string_bonjour
-
-
-age = 17
-puts "You are #{age + 1} years old"
-#=> you are 18 years old
 
 # ******************************************
 # METHODS
 # PROBLEM, if i want to change the sentence( adding content) and greet 25 people, i need to write it 25 times...long and boring.. and i will be repeating myself..
 
+puts "Hello Joanna!"
+puts "Hello Corey!"
+puts "Hello Anthony!"
 
-puts "Hello, Lukas!"
-puts "Hello,Meder!"
-puts "Hello, Ashley!"
-#=> very repetitive, Don't repeat Yourself(DRY CONVENTION)
+
+# => very repetitive, Dont repeat Yourself(DRY CONVENTION)
+# reuse methods over time in the program
+# reuse/re-assign variables over time
+
 
 # METHODS ARE A way of factorising your code. A method can have parameters or not => CONVENTION IN CODE: Don't Repeat Yourself
 
@@ -64,51 +71,48 @@ puts "Hello, Ashley!"
 # a method ALWAYS returns the last line, therefore return keyword is implicit/not mandatory.
 
 # definition/theory
-def saying_hello(name)
-  #do something :body of the method, process something
-    "hello, #{name}!"
-  #return something :output/result of the method
+def name_of_the_method(parameter_one, parameter_two)
+#   do something body of the method, process something
+#     ...
+#     ...
+#     ...
+  return something output/result of the method
 end
 
 # Calling a method means executing a method
 # you execute a method with arguments
-# name_of_the_method(argument)
-
-puts saying_hello("philip")
-puts saying_hello("lewagon")
-
-# Parameters are variables waiting to be assigned
-# Argument is a variable, the value that you give to parameters
+puts name_of_the_method(argument_one, argument_two)
 
 
-puts "what is your name?"
-name_user = gets.chomp
+#Important definition:
 
-puts saying_hello(name_user)
-
-def full_name(first_name, last_name)
-  capitalized_first_name = first_name.capitalize
-  capitalized_last_name = last_name.capitalize
-  return "#{capitalized_first_name}"
-end
-
-puts full_name("Fanny","Rojon" )
+# Parameters are variables waiting to be assigned. Empty box with only a name/label
+# Argument is a variable, the value that you give to parameters. Box with a name + an object inside.
 
 
 # ***********************************************
 # METHOD WITHOUT PARAMETERS
 
-def tomorrow_date
-  return (Date.today + 1).strftime('%b %d, %Y')
+def greetings(first_name, last_name)
+  # capitalizing the first name
+  # capitalized_first_name = first_name.capitalize
+  # capitalizing the last name
+  # capitalized_last_name = last_name.capitalize
+  # return a string with greetings for the person
+  "Hi, welcome #{first_name.capitalize} #{last_name.capitalize}"
+  # return greeting to people
 end
 
-puts tomorrow_date
+# Execute/calling the method
+puts greetings('olivia', 'wagon')
+puts greetings('Sjors', 'amsterdam')
 
 
 
 # IMPORTANT:
 # -variables and methods always lower_case and english
 # -A method always returns the last line, so return is implicit
+
 
 
 
